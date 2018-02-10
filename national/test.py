f650ccfceb1bdba0416fe638f845c2c9b6c6cbe4
Nikhil Milind <nmilind2@ncsu.edu>
2018-02-09 23:02:10 -0500

    Generated Tesla Car Location Coordinations

--- national/test.py
+++ national/test.py
@@ -2,15 +2,16 @@ from geopy.geocoders import Nominatim
 from geopy.geocoders import GoogleV3
 from geopy.distance import vincenty
 from geopy.distance import great_circle
+import time
 
 def main():
     geolocator = Nominatim()
-    googleAPI = GoogleV3(api_key="AIzaSyAQAiLktW0gJ9COS2stDmxuc_dZRCzcf-k")
     locations = read_csv("../data/TeslaCarLocations.csv")
     with open("../data/TeslaCarLocations_Coords.csv", "w") as file:
         for k in range(len(locations)):
             print(locations[k])
             try:
+                time.sleep(0.5)
                 location = geolocator.geocode(locations[k])
                 coordinates = (location.latitude, location.longitude)
                 file.write("\"%s\",\"%s\",\"%s\"\n" % (locations[k], coordinates[0], coordinates[1]))
